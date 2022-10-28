f018584f595b98f64e3b3a4993ad88911adff63c
Josh Suson <digital@ilovetheory.com>
2022-10-28 15:39:56 -0400

    Make copy changes plus other small details

--- pages/digital/index.js
+++ pages/digital/index.js
@@ -42,8 +42,11 @@ export default function Digital() {
         </div>
         <div className="flex flex-col lg:flex-row gap-12 md:gap-24 w-3/4 mx-auto">
           <Bottle />
-          <p className="lg:w-11/12 text-center md:text-left text-lg md:text-3xl font-light self-center">{`Did you know that Mobil 1 Supercar 0W-40 is the first and only dexos-approved motor oil developed specifically for Corvette? The technology that helped produce every Corvette Racing win ends up in a motor oil that guarantees Corvette power and precision.*`}</p>
+          <p className="lg:w-11/12 text-center md:text-left text-lg md:text-3xl font-light self-center">{`Did you know that Mobil 1 Supercar 0W-40 is the first and only dexos-approved motor oil developed specifically for Corvette? With the LT1 E-ROD Connect & Cruise powertrain system, the heart of every Corvette can motivate your hot rod to get out of the garage and onto the street.*`}</p>
         </div>
+        <p className="text-center text-sm text-gray-400 mt-4">
+          *Mobil 1 bottle custom label not found elsewhere.
+        </p>
         <div id="formContainer" className="my-24 max-w-2xl mx-auto">
           <h2 className="text-4xl font-bold text-center mb-12">
             Sign up, stay tuned
